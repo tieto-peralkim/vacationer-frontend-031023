9cf38429e19128ddfff2d168296f3a1e7660a195
Kimmo Perälä <kimmo.perala@tietoevry.com>
2022-10-04 16:53:51 +0300

    Fixed docker run command

--- docker_restart.sh
+++ docker_restart.sh
@@ -11,12 +11,12 @@ docker rm vacationer-"$1";
 echo "Run the new $1 image"
 if [[ "$1" == "backend" ]] ; then
 	ports=3001:3001
-	envVariables=
+	docker run -d -p $ports --name=vacationer-"$1" 629517020360.dkr.ecr.eu-west-1.amazonaws.com/ecr-vacationer-"$1":latest
 else
   ports=80:80
   envVariables="REACT_APP_ADDRESS=http://34.240.254.109:3001"
+  docker run -d -p $ports -e $envVariables --name=vacationer-"$1" 629517020360.dkr.ecr.eu-west-1.amazonaws.com/ecr-vacationer-"$1":latest
 fi
-docker run -d -p $ports -e $envVariables --name=vacationer-"$1" 629517020360.dkr.ecr.eu-west-1.amazonaws.com/ecr-vacationer-"$1":latest
 echo "Docker run done, exiting EC2"
 exit
 else
