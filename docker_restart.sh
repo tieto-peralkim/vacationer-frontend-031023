17cb8ad970c151427a05a2da684dfea8b018c932
Kimmo Perälä <kimmo.perala@tietoevry.com>
2022-10-10 16:00:35 +0300

    Changed app address to env variable

--- docker_restart.sh
+++ docker_restart.sh
@@ -11,11 +11,14 @@ docker rm vacationer-"$1";
 echo "Run the new $1 image"
 if [[ "$1" == "backend" ]] ; then
 	ports=3001:3001
-  envVariables="REACT_APP_ADDRESS=http://34.240.254.109:3001"
-  docker run -d -p $ports -e $envVariables --name=vacationer-"$1" 629517020360.dkr.ecr.eu-west-1.amazonaws.com/ecr-vacationer-"$1":latest
+  envVariable1=(REACT_APP_ADDRESS="$2")
+  envVariable2=(REACT_APP_MONGODB_URI="$3")
+  envVariable3=(REACT_APP_SLACK_URI="$4")
+  docker run -d -p $ports -e "${envVariable1[@]}" -e "${envVariable2[@]}" -e "${envVariable3[@]}" --name=vacationer-"$1" 629517020360.dkr.ecr.eu-west-1.amazonaws.com/ecr-vacationer-"$1":latest
 else
   ports=80:80
-  docker run -d -p $ports --name=vacationer-"$1" 629517020360.dkr.ecr.eu-west-1.amazonaws.com/ecr-vacationer-"$1":latest
+  envVariable1=(REACT_APP_ADDRESS="$2")
+  docker run -d -p $ports -e "$envVariable1" --name=vacationer-"$1" 629517020360.dkr.ecr.eu-west-1.amazonaws.com/ecr-vacationer-"$1":latest
 fi
 echo "Docker run done, exiting EC2"
 exit
