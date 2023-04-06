3c76ae2789aaa795b53fd82944a2de5e35cf5b06
Kimmo Perälä <kimmo.perala@tietoevry.com>
2023-04-06 12:13:19 +0300

    103 CSS changes

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -47,9 +47,7 @@ export default function Login() {
                                 </AccordionSummary>
                                 <Divider />
                                 <AccordionDetails
-                                    sx={{
-                                        bgcolor: "orange",
-                                    }}
+                                    className={styles.accordionDetails}
                                 >
                                     <div>
                                         <ol>
