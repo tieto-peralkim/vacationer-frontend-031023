4c3dcbdec7f0ef913f3c8eeb899b346d9d101112
Kimmo Perälä <kimmo.perala@tietoevry.com>
2023-09-26 10:19:39 +0300

    Remove unneeded local env variable

--- src/NavigationBar.tsx
+++ src/NavigationBar.tsx
@@ -29,6 +29,9 @@ import { PersonPin } from "@mui/icons-material";
 import Login from "./pages/Login/Login";
 import CustomAlert from "./components/CustomAlert";
 import { Buffer } from "buffer";
+const isQaOrProdVersion =
+    process.env.REACT_APP_ENVIRONMENT === "QA" ||
+    process.env.REACT_APP_ENVIRONMENT === "PROD";
 
 export interface Vacationer {
     id: string;
@@ -331,7 +334,12 @@ function NavigationBar() {
                             Report bugs / ideas
                         </a>
                     </div>
-                    <div>Version: {process.env.REACT_APP_VERSION}</div>
+                    <div>
+                        Version:{" "}
+                        {isQaOrProdVersion
+                            ? process.env.REACT_APP_VERSION
+                            : process.env.REACT_APP_ENVIRONMENT}
+                    </div>
 
                     <div className={styles.footerRight}>
                         Built by Tietoevry Cloud Digital Operations
